36f6633f22b95c12e363c531b03afc88a31b7ece
almeidadylan <almeidadylan793@gmail.com>
2022-08-01 14:06:17 -0400

    error: conflitos

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import Footer from '../../components/footer/footer';
+//import Footer from '../../components/footer/footer';
 import Header from '../../components/Header/index';
 
 function Home () {
